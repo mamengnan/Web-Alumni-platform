760c9e8561e720b0bb93f4da3d2320438adb800b
FengTao <759078664@qq.com>
2018-01-26 22:33:22 +0800

    开发环境下使用vue.js,部署时配置vue.min.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import router from './routers'
 import store from './pages/vuex/store.js'
 
 //设置为 false 以阻止 vue 在启动时生成生产提示。
-Vue.config.productionTip = true
+Vue.config.productionTip = false
 
 Vue.use(VueResource);
 
