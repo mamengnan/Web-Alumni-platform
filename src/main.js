b6db2074b5ee2181ef87a402fc46a8a06c8f6bd2
FengTao <759078664@qq.com>
2018-01-27 16:52:36 +0800

    .

--- src/main.js
+++ src/main.js
@@ -6,13 +6,12 @@ import App from './App'
 import router from './routers'
 import store from './pages/vuex/store.js'
 
-console.log("%c", "color:blue;font-weight:bold;");
-console.log("%c _   _ ____ _____ ____   ____                            _____ ", "color:blue;font-weight:bold;");
-console.log("%c| | | / ___|_   _/ ___| |  _ \\ _ __ __ _  __ _  ___  _ _|_   _|__  __ _ _ __ ___", "color:blue;font-weight:bold;");
-console.log("%c| | | \\___ \\ | || |     | | | | '__/ _` |/ _` |/ _ \\| '_ \\| |/ _ \\/ _` | '_ ` _ \\", "color:blue;font-weight:bold;");
-console.log("%c| |_| |___) || || |___  | |_| | | | (_| | (_| | (_) | | | | |  __/ (_| | | | | | |", "color:blue;font-weight:bold;");
-console.log("%c \\___/|____/ |_| \\____| |____/|_|  \\__,_|\\__, |\\___/|_| |_|_|\\___|\\__,_|_| |_| |_|", "color:blue;font-weight:bold;");
-console.log("%c                                         |___/ Release 0.0.1. Powered by 主站前端.", "color:blue;font-weight:bold;");
+console.log("%c _   _ ____ _____ ____   ____                            _____ \n" +
+  "| | | / ___|_   _/ ___| |  _ \\ _ __ __ _  __ _  ___  _ _|_   _|__  __ _ _ __ ___\n" +
+  "| | | \\___ \\ | || |     | | | | '__/ _` |/ _` |/ _ \\| '_ \\| |/ _ \\/ _` | '_ ` _ \\\n" +
+  "| |_| |___) || || |___  | |_| | | | (_| | (_| | (_) | | | | |  __/ (_| | | | | | |\n" +
+  " \\___/|____/ |_| \\____| |____/|_|  \\__,_|\\__, |\\___/|_| |_|_|\\___|\\__,_|_| |_| |_|\n" +
+  "                                         |___/ Release 0.0.1. Powered by 科大软院 DragonTeam.", "color:rgb(18,143,205);font-weight:bold;");
 
 //设置为 false 以阻止 vue 在启动时生成生产提示。
 Vue.config.productionTip = false
